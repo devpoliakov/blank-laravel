bccd9a4b628c6142716173cbc0093749b071a60e
Yurii Poliakov <kontynent@ua.fm>
2021-07-26 10:16:09 +0300

    Registration: disable routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//Route::resource('/companies', Companies::class);
+
+Auth::routes(['register' => false]);
+
+Route::get('/home', 'HomeController@index')->name('home');
