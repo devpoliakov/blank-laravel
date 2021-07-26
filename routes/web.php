fd51412ec9a2534f4013870c0e73a413c1a97362
Yurii Poliakov <kontynent@ua.fm>
2021-07-26 15:16:09 +0300

    Companies: update store routes

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::get('/', 'HomeController@index')->name('home');
 // Companies
 Route::get('/companies', 'Companies@index')->name('companies');
 Route::get('/companies/create', 'Companies@create')->name('companiesCreate');
+Route::post('/companies/store', 'Companies@store')->name('companiesCreate');
 
 Auth::routes(['register' => false]);
 
